27a362940f65929b10a38e4e671ef6c0f485ad50
Yonatan Raymundo <yraymundo.dev@gmail.com>
2026-03-19 19:32:13 +0000

    Elimina system-monitor

--- system-monitor/scripts/analizar_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FECHA=$(date +%Y-%m-%d_%H-%M-%S)
-
-echo "===== ANALISIS DE LOGS =====" > ../output/errores_$FECHA.txt
-
-grep ERROR ../logs/*.log >> ../output/errores_$FECHA.txt
-
-echo "Total de errores encontrados:" >> ../output/errores_$FECHA.txt
-grep ERROR ../logs/*.log | wc -l   >> ../output/errores_$FECHA.txt
